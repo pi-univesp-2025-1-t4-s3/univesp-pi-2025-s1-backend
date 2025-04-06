0debd82c358fc9c8303a785a5410414cf0797604
Richard Cardoso <richard.166@hotmail.com>
2025-04-05 22:30:19 -0300

    fix: configurando cors

---
+++ src/main/java/com/univesp/pi/pji310/s3/t4/pi_2025_s1/config/security/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.univesp.pi.pji310.s3.t4.pi_2025_s1.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
