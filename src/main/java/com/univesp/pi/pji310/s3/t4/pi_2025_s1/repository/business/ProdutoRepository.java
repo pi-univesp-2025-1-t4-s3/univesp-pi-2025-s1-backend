3c9fdf2ba4d592a25ce1fe9b22b2f09a4322b3f2
Richard Cardoso <richard.166@hotmail.com>
2025-05-11 11:31:17 -0300

    feat: adicionando endpoint para buscar produtos por nome

--- src/main/java/com/univesp/pi/pji310/s3/t4/pi_2025_s1/repository/business/ProdutoRepository.java
+++ src/main/java/com/univesp/pi/pji310/s3/t4/pi_2025_s1/repository/business/ProdutoRepository.java
@@ -2,6 +2,10 @@ package com.univesp.pi.pji310.s3.t4.pi_2025_s1.repository.business;
 
 import com.univesp.pi.pji310.s3.t4.pi_2025_s1.domain.business.Produto;
 
+import java.util.List;
+
 public interface ProdutoRepository
         extends BaseRepository<Produto> {
+
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
 }
